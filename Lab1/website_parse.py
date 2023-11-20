13b31d67f9a0b8056e453b101eb9b5805fbdd883
maeosha <mochkauha123@gmail.com>
2023-11-21 00:53:56 +0400

    getting data from the site

--- Lab1/website_parse.py
+++ Lab1/website_parse.py
@@ -11,6 +11,32 @@ count = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0}
 def getting_full_review():
     sleep(5)
     buttons = driver.find_elements(By.CSS_SELECTOR, "a.read-more__link")
+    for elem in range(len(buttons)):
+        sleep(1)
+        driver.execute_script(f'document.getElementsByClassName("read-more__link")[{elem}].click()')
+
+    books = driver.find_elements(By.CSS_SELECTOR, "article.review-card.lenta__item ")
+    for book in books:
+        book_title = book.find_element(By.CSS_SELECTOR, "a.lenta-card__book-title").text
+
+        try:
+            authors_rating = book.find_element(By.CSS_SELECTOR, "span.lenta-card__mymark").text
+        except NoSuchElementException:
+            continue
+        if count[int(float(authors_rating) // 1)] >= 1001:
+            continue
+
+        if book.find_element(By.CSS_SELECTOR, "div.lenta-card__rating"):
+            overall_rating = book.find_element(By.CSS_SELECTOR, "div.lenta-card__rating").text
+        else:
+            continue
+
+        if book.find_elements(By.ID, "lenta-card__text-review-full"):
+            full_review = book.find_element(By.ID, "lenta-card__text-review-full").text
+        else:
+            full_review = book.find_element(By.ID, "lenta-card__text-review-escaped").text
+
+        count[int(float(authors_rating) // 1)] += 1
 
 
 while sum(count) <= 6000:

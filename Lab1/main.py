a1114dcc8714ab2e5b795bdf6569c18acb37a2b3
maeosha <mochkauha123@gmail.com>
2023-11-28 17:03:02 +0400

    I hope that I have corrected everything

--- Lab1/main.py
+++ Lab1/main.py
@@ -15,14 +15,26 @@ parser.add_argument('--url',
                     default="https://www.livelib.ru/reviews/~2",
                     help='address of the LiveLib page')
 
+parser.add_argument('--dir_name',
+                    type=str,
+                    default="data_set",
+                    help='directory name')
+
+parser.add_argument('--max_txt_files',
+                    type=int,
+                    default=1001,
+                    help='maximum number of files per evaluation')
+
+
 my_variables = parser.parse_args()
 
 if __name__ == "__main__":
     count = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0}
     page = my_variables.page
     url = my_variables.url
-
-    start_parse(page, count, url)
+    dir_name = my_variables.dir_name
+    max_txt_files = my_variables.max_txt_files
+    start_parse(page, count, url, dir_name, max_txt_files)
 
 
 
--- Lab1/website_parse.py
+++ Lab1/website_parse.py
@@ -1,42 +1,32 @@
 import os
-
+import logging
 from time import sleep
-
 from typing import Dict
-
+import void
 from selenium import webdriver
-
 from selenium.webdriver.chrome.webdriver import WebDriver
-
 from selenium.webdriver.common.by import By
-
 from selenium.common.exceptions import NoSuchElementException
 
 
-def creating_files():
+def creating_files(dir_name) -> void:
     """сreates a dataset using the os library"""
     try:
-        os.mkdir(os.path.join("data_set"))
-    except:
-        print("Error! A file with this name already exists!")
-        print("Do you want to continue in this file?")
-        print("1 - yes, 2 - no")
-        answer = int(input())
-        if answer == 2:
-            quit()
+        os.mkdir(os.path.join(dir_name))
+    except NameError:
+        logging.warning("Error! A file with this name already exists!", NameError)
 
     for i in range(0, 6):
-        if not os.path.isdir(os.path.join("data_set", f"{i} stars")):
-            os.mkdir(os.path.join("data_set", f"{i} stars"))
+        if not os.path.isdir(os.path.join(dir_name, f"{i} stars")):
+            os.mkdir(os.path.join(dir_name, f"{i} stars"))
 
 
-def creating_file_name(authors_rating: float, count: Dict[int, int]) -> str:
+def creating_file_name(authors_rating: float, count: Dict[int, int], dir_name: str) -> str:
     """creating a file name for further work"""
     zeros: Dict[int, str] = {1: "000", 2: "00", 3: "0", 4: ""}
     number_of_reviews: int = count[int(authors_rating)]
     number_of_stars: int = int(authors_rating)
-    number_of_zeros: str = zeros[len(str(number_of_reviews))]
-    file_name: str = os.path.join("data_set", f"{number_of_stars} stars", f"{number_of_zeros}{number_of_reviews}")
+    file_name: str = os.path.join(dir_name, f"{number_of_stars} stars", f"{number_of_reviews:04}")
     return file_name
 
 
@@ -49,7 +39,7 @@ def write_to_fail(book_title: str, overall_rating: str, authors_rating: float, f
         file_review.write(f"Отзыв: {full_review}\n")
 
 
-def getting_full_review(driver: WebDriver, count: Dict[int, int]):
+def getting_full_review(driver: WebDriver, count: Dict[int, int], dir_name: str, max_txt_files: int) -> void:
     """getting information from the site and transmitting this data to the functions"""
     sleep(5)
     buttons = driver.find_elements(By.CSS_SELECTOR, "a.read-more__link")
@@ -68,7 +58,7 @@ def getting_full_review(driver: WebDriver, count: Dict[int, int]):
         except NoSuchElementException:
             continue
 
-        if count[int(float(authors_rating))] >= 1001:
+        if count[int(float(authors_rating))] >= max_txt_files:
             continue
 
         if book.find_element(By.CSS_SELECTOR, "div.lenta-card__rating"):
@@ -81,19 +71,19 @@ def getting_full_review(driver: WebDriver, count: Dict[int, int]):
         else:
             full_review: str = book.find_element(By.ID, "lenta-card__text-review-escaped").text
 
-        file_name: str = creating_file_name(authors_rating, count)
+        file_name: str = creating_file_name(authors_rating, count, dir_name)
         write_to_fail(book_title, overall_rating, authors_rating, full_review, file_name)
-        count[int(float(authors_rating) // 1)] += 1
+        count[int(float(authors_rating))] += 1
 
 
-def start_parse(page: int, count: Dict[int, int], url: str):
+def start_parse(page: int, count: Dict[int, int], url: str, dir_name: str, max_txt_files: int) -> void:
     """the beginning of parsing"""
-    creating_files()
+    creating_files(dir_name)
 
-    while sum(count) <= 6000:
+    while sum(count) <= max_txt_files * 6:
         url = f"{url[:-1]}{page}"
         driver: WebDriver = webdriver.Chrome()
         driver.get(url)
-        getting_full_review(driver, count)
+        getting_full_review(driver, count, dir_name, max_txt_files)
         page += 1
 

86bffae3d8a665bba0dbb02ec7cc85f6349aa2f6
maeosha <mochkauha123@gmail.com>
2023-12-17 22:44:35 +0400

    filtering and obtaining statistical information

--- Lab4/test.py
+++ Lab4/test.py
@@ -20,12 +20,34 @@ def make_data_frame(path_csv: str) -> pd.DataFrame:
                 text = ''
                 for index in range(3, len(lines)):
                     text += lines[index]
-                texts.append(text)
+
+                texts.append(text[6:])
                 count_words.append(len(text))
-    df = pd.DataFrame({'review': texts, 'count words': count_words, 'stars': stars})
+    data_frame = pd.DataFrame({'review': texts, 'number of words': count_words, 'stars': stars}).dropna()
 
     logging.info("DataFrame was created!")
 
-    return df
+    return data_frame
+
+
+def get_stats_information(data_frame: pd.DataFrame):
+    return data_frame.describe()
+
+
+def filter_by_number_of_words(data_frame: pd.DataFrame, limit_value: int):
+    return data_frame[data_frame["number of words"] <= limit_value]
+
+
+def filter_by_stars(data_frame: pd.DataFrame, stars: int):
+    if stars < 0 or stars > 5:
+        logging.warning("Invalid number of stars!")
+    else:
+        return data_frame[data_frame["stars"] == stars]
+
+
+def get_stats_information(data_frame: pd.DataFrame, stars: int):
+    return data_frame.groupby('stars').agg({'number of words': ['max', 'min', 'mean']})
+
+
 
 

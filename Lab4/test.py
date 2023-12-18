c35c816e12f08c3dc03c58920ae1a0c849739d93
maeosha <mochkauha123@gmail.com>
2023-12-18 15:09:49 +0400

    split the code and added entry points

--- Lab4/test.py
+++ Lab4/test.py
@@ -1,69 +1,23 @@
 import logging
 
-import pandas as pd
-import csv
-
 import void
-from nltk import SnowballStemmer, WordNetLemmatizer
-from nltk.tokenize import word_tokenize
 import matplotlib.pyplot as plt
 import argparse
+
+from nltk import SnowballStemmer, WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+
+from data_frame import create_data_frame
+from data_frame import filter_by_stars
 
 
 logging.basicConfig(level=logging.INFO)
 
 
-def create_data_frame(path_csv: str) -> pd.DataFrame:
-    """The function creates a date frame"""
-    texts: list = []
-    stars: list = []
-    count_words: list = []
-    with open(path_csv, 'r', encoding='utf-8') as csvfile:
-        reader = csv.reader(csvfile, delimiter=",")
-        reader.__next__()
-        for row in reader:
-            stars.append(int(row[2]))
-            with open(row[0], 'r', encoding='utf-8') as txtfile:
-                lines = txtfile.readlines()
-                text = ''
-                for index in range(3, len(lines)):
-                    text += lines[index]
-
-                texts.append(text[6:])
-                count_words.append(len(text))
-    data_frame = pd.DataFrame({'review': texts, 'number of words': count_words, 'stars': stars}).dropna()
-
-    logging.info("DataFrame was created!")
-
-    return data_frame
-
-
-def get_stats_information(data_frame: pd.DataFrame) -> pd.DataFrame:
-    """The function receives statistical information"""
-    return data_frame.describe()
-
-
-def filter_by_number_of_words(data_frame: pd.DataFrame, limit_value: int) -> pd.DataFrame:
-    """The function filters by the number of words"""
-    return data_frame[data_frame["number of words"] <= limit_value]
-
-
-def filter_by_stars(data_frame: pd.DataFrame, stars: int) -> pd.DataFrame:
-    """The function filters by the number of stars"""
-    if stars < 0 or stars > 5:
-        logging.warning("Invalid number of stars!")
-    else:
-        return data_frame[data_frame["stars"] == stars]
-
-
-def get_stats_information(data_frame: pd.DataFrame, stars: int) -> pd.DataFrame:
-    """The star-filtered function receives statistical information"""
-    data_frame = filter_by_stars(data_frame, stars)
-    return data_frame.agg({'number of words': ['max', 'min', 'mean']})
-
-
-def create_histogram(data_frame: pd.DataFrame, stars: int) -> list:
+def create_histogram(path_csv: str, stars: int) -> list:
     """The function creates a histogram"""
+    data_frame = create_data_frame(path_csv)
+
     stemmer = SnowballStemmer("russian")
     lemmatizer = WordNetLemmatizer()
     lemmas = []
@@ -73,12 +27,13 @@ def create_histogram(data_frame: pd.DataFrame, stars: int) -> list:
         for word in word_tokenize(review):
             if ord(word[0]) > 191:
                 lemmas.append(lemmatizer.lemmatize(word))
+    logging.info("Histogram have created!")
     return lemmas
 
 
-def draw_histogram(data_frame: pd.DataFrame) -> void:
+def draw_histogram(path_csv: str) -> void:
     """The function draws a histogram"""
-    plt.bar(range(0, 6), [len(create_histogram(df, i)) for i in range(0, 6)])
+    plt.bar(range(0, 6), [len(create_histogram(path_csv, stars)) for stars in range(0, 6)])
     plt.xlabel("Number of stars")
     plt.ylabel("Number of words in review")
     plt.legend()
@@ -87,10 +42,9 @@ def draw_histogram(data_frame: pd.DataFrame) -> void:
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Input csv path, label of class")
-    parser.add_argument( "--csv", help="Input csv path", default="data_csv", type=str)
+    parser.add_argument("--csv", help="Input csv path", default="data_csv", type=str)
     args = parser.parse_args()
-    df = create_data_frame(args.csv)
-    draw_histogram(df)
+    draw_histogram(args.csv)
 
 
 
